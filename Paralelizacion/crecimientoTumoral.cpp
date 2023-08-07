4bf8d4243408b53c47628d86a809d5fd92654387
Luisrd <luisrd@correo.ugr.es>
2023-08-07 12:27:29 +0200

    Inicializacion de matrices de indices y celulas

--- Paralelizacion/crecimientoTumoral.cpp
+++ Paralelizacion/crecimientoTumoral.cpp
@@ -1,12 +1,20 @@
 #include <iostream>
+#include <vector>
 #include "random.hpp"
+
 using namespace std;
 
 struct Celula {
+	bool cancer = false;
 	float cct;
 	float ro;
 	float mu;
 	float alpha;
+};
+
+struct Coordenadas{
+	int x;
+	int y;
 };
 // Probablemente funciones
 struct Opciones {
@@ -27,8 +35,8 @@ static int ROMAX = 10;
 static float ALPHAMAX = 0.01;
 
 static int LONG = 201;
-
-void introducir_en_casilla( int casilla, Celula celula){
+/*
+void introducir_en_casilla( int i, int j, int casilla, Celula celula){
 	if (casilla == 0)
         	rejilla[i - 1][j - 1] = celula;
     	else if (casilla == 1)
@@ -47,37 +55,62 @@ void introducir_en_casilla( int casilla, Celula celula){
         	rejilla[i - 1][j] = celula;
 }
 
-int* casillas_libres( int i, int j){
-	int libres[8];
-	for( int h = 0; h < 8; h++){
-		libres[h] = h;
-	}
+vector<int> casillas_libres( int i, int j){
+	vector<int> libres;
 	
 	if ( i-1 >= 0 && j-1 >= 0 && i-1 < LONG && j-1 < LONG && !( type(rejilla[i-1][j-1]) == Celula) )
-       	libres.append(0);
+       	libres.push_back(0);
 	if ( i >= 0 && j-1 >= 0 && i < LONG && j-1 < LONG && !( type(rejilla[i][j-1]) == Celula) )
-       	libres.append(1);
+       	libres.push_back(1);
 	if ( i+1 >= 0 && j-1 >= 0 && i+1 < LONG && j-1 < LONG && !( type(rejilla[i+1][j-1]) == Celula) )
-       	libres.append(2);
+       	libres.push_back(2);
 	if ( i+1 >= 0 && j >= 0 && i+1 < LONG && j < LONG && !( type(rejilla[i+1][j]) == Celula) )
-       	libres.append(3);
+       	libres.push_back(3);
 	if ( i+1 >= 0 && j+1 >= 0 && i+1 < LONG && j+1 < LONG && !( type(rejilla[i+1][j+1]) == Celula) )
-       	libres.append(4);
+       	libres.push_back(4);
 	if ( i >= 0 && j+1 >= 0 && i < LONG && j+1 < LONG && !( type(rejilla[i][j+1]) == Celula) )
-       	libres.append(5);
+       	libres.push_back(5);
 	if ( i-1 >= 0 && j+1 >= 0 && i-1 < LONG && j+1 < LONG && !( type(rejilla[i-1][j+1]) == Celula) )
        	libres.append(6);
 	if ( i-1 >= 0 && j >= 0 && i-1 < LONG && j < LONG && !( type(rejilla[i-1][j]) == Celula) )
-       	libres.append(7);
+       	libres.push_back(7);
 	
 	return libres;
 }
-
-
+*/
 int main(){
+
 	cout << "Hello World" << endl;
 	
-	return 0;
+	vector <Coordenadas> matriz(LONG*LONG);
+	Coordenadas coor;
+		
+	for( int i = 0; i < LONG; i++ ){
+		coor.x = i;
+		coor.y = i;
+		matriz[i*LONG+i] = coor;
+		for( int j = i+1; j < LONG; j++){
+			coor.x = i;
+			coor.y = j;
+			matriz[i*LONG + j] = coor;
+			coor.x = j;
+			coor.y = i;
+			matriz[j*LONG + i] = coor;
+		}
+	}	
+	//for ( int i = 0; i < matriz.size(); i++)
+	//	cout <<"("<< matriz[i].x << "," << matriz[i].y << ")-\t";
+	
+	vector < vector <Celula> > rejilla(LONG);
+	for( int i = 0; i < LONG; i++)
+		rejilla[i] = vector <Celula> (LONG);
+	
+	/*if( rejilla[20][3].cancer )	
+		cout << "Cancer" << endl;
+	else
+		cout << "Sana" << endl;
+		
+	return 0;*/
 }
 /**
     "def seleccionar_casilla(tabla):\n",
